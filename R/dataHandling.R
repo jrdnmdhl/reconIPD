1cd371ba4d858a2e2b06734cea182c0599e8ad9c
jrdnmdhl <jrdnmdhl@gmail.com>
2016-03-17 09:26:14 -0400

    First update

---
+++ R/dataHandling.R
@@ -0,0 +1,62 @@
+#' doRiskIndex
+#' @export
+doRiskIndex = function(nrisk, survDf){
+  nRiskIndexed = adply(
+    nrisk,
+    1,
+    function(d){
+      priorSurv = survDf %>% filter(survDf$t >= d$Time)
+      nRows = nrow(priorSurv)
+      d$lower = head(priorSurv, 1)$i
+      return(d)
+    }
+  )
+  nRiskIndexed = nRiskIndexed %>% mutate(
+    upper = lead(lower) - 1,
+    t = Time,
+    i = seq_len(nrow(nRiskIndexed)),
+    n = N
+  )
+  nRiskIndexed[nrow(nRiskIndexed), ]$upper = max(survDf$i)
+  nRiskIndexed = nRiskIndexed %>% select(i, t, lower, upper, n)
+
+  return(nRiskIndexed)
+}
+
+#' doSurvFix
+#' @export
+doSurvFix = function(survDf){
+  nRow = nrow(survDf) + 1
+  tempSurv = rbind.fill(
+    survDf,
+    data.frame(
+      Time = 0,
+      Probability = 1
+    )
+  ) %>%
+    arrange(Time) %>%
+    mutate(
+      t = Time,
+      i = seq_len(nRow)
+    )
+  tempSurv$s = 1
+  for(i in seq_len(nrow(tempSurv) - 1)){
+    tempSurv$s[i+1] = min(tempSurv$s[i], tempSurv$Probability[i+1])
+  }
+  outSurv = tempSurv %>% select(i, t, s)
+  return(outSurv)
+}
+
+#' reconIPD
+#' @export
+reconIPD = function(path, tags = list()){
+  survData = read.xlsx(path, "Survival") %>% doSurvFix
+  riskData = read.xlsx(path, "Number at Risk") %>% doRiskIndex(survData)
+  miscInputs = read.xlsx(path, "Misc")
+  if(!is.na(miscInputs[1,1])) totEvents = miscInputs[1,2]
+  else if(!is.na(miscInputs[1,2])) totEvents = riskData$N - miscInputs[1,2]
+  else totEvents = "NA"
+  guyot(survData, riskData, tot.events = totEvents, tags)
+}
+
+
